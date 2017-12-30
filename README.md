74b7f1ae366e2fd664dbbf3ef474580e6142e598
deepak1393 <deepak1393@gmail.com>
2017-12-30 22:55:42 +0530

    Youtube Search Application | My first commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ReduxSimpleStarter
 
-Interested in learning [Redux](https://www.udemy.com/react-redux/)?
+Welcome to deepak1393 Github Repo!
 
 ### Getting Started
 
@@ -10,14 +10,14 @@ There are two methods for getting started with this repo.
 Checkout this repo, install dependencies, then start the gulp process with the following:
 
 ```
-> git clone https://github.com/StephenGrider/ReduxSimpleStarter.git
-> cd ReduxSimpleStarter
+> git https://github.com/deepak1393/youtube-search.git
+> cd youtube-search
 > npm install
 > npm start
 ```
 
 #### Not Familiar with Git?
-Click [here](https://github.com/StephenGrider/ReactStarter/releases) then download the .zip file.  Extract the contents of the zip file, then open your terminal, change to the project directory, and:
+Click [here](https://github.com/deepak1393/youtube-search/releases) then download the .zip file.  Extract the contents of the zip file, then open your terminal, change to the project directory, and:
 
 ```
 > npm install
--- src/index.js
+++ src/index.js
@@ -1,15 +1,61 @@
-import React from 'react';
+import _ from 'lodash';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search';
+const API_KEY = 'AIzaSyA931SdrBmwgLezpO1JqhTDe1DZCKK3oKI';
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    this.videoSearch('God');
+  }
+
+  videoSearch(term){
+    YTSearch({key:API_KEY, term:term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+
+  debounce(cb, wait){
+      var timeout;
+      return function(term){
+        if(timeout){
+           clearTimeout(timeout);
+        }
+        timeout = setTimeout(cb, wait, term);
+      }
+  }
+
+  render(){
+    const videoSearch = this.debounce((term) => { this.videoSearch(term) }, 300);
+    return (
+      <div>
+        <SearchBar
+        // onSearchTermChange = {function(term){
+        //   this.videoSearch(term);
+        // }}
+          //onSearchTermChange={term => this.videoSearch(term)} />
+          onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
